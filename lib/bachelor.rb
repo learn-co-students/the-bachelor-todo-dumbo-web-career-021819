9f8ed222298b2afe2356f355cd01a5fb0d0c4e25
William Robert Simpson <will.simpson85@gmail.com>
2019-01-30 23:06:12 +0000

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,9 +1,27 @@
+require 'pry'
 def get_first_name_of_season_winner(data, season)
-  # code here
+winner = ""
+data[season].each do |x|
+if x["status"] == "Winner"
+  winner = x["name"].split(" ").first
 end
+end
+winner
+end
+
 
 def get_contestant_name(data, occupation)
   # code here
+  name = ""
+  data.each do |k,v|
+    v.each do |x|
+      if x["occupation"] == occupation
+        name = x["name"]
+      #binding.pry
+    end
+    end
+  end
+  name
 end
 
 def count_contestants_by_hometown(data, hometown)
