f03d128f6ab5cae578311c17b381b1b06c1dbe51
Ryan Ho <rhohoman@gmail.com>
2019-02-15 21:04:40 +0000

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,6 +1,22 @@
+require "pry"
 def get_first_name_of_season_winner(data, season)
   # code here
+  winner = ""
+  
+  data.each do |season, data_types|                 #hashes iteration
+    data_types.each do |person|                     #array iteration
+      
+      if person["status"] == "Winner"               #conditional for winner
+        winner = person["name"]                     #assign winner name to variable
+        binding.pry
+      end
+      
+    end
+  end
+  winner.split(" ").shift
 end
+
+
 
 def get_contestant_name(data, occupation)
   # code here
