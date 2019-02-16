b90ca42adefe028ebcc001bfe51794b47c9ee23c
Ryan Ho <rhohoman@gmail.com>
2019-02-16 18:06:56 +0000

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -3,16 +3,25 @@ def get_first_name_of_season_winner(data, season)
   # code here
   winner = ""
   
-  data.each do |season, data_types|                 #hashes iteration
-    data_types.each do |person|                     #array iteration
+  # data.each do |season, data_types|                 #hashes iteration
+  #   data_types.each do |person|                     #array iteration
       
-      if person["status"] == "Winner"               #conditional for winner
-        winner = person["name"]                     #assign winner name to variable
-        binding.pry
-      end
+  #     if person["status"] == "Winner"               #conditional for winner
+  #       winner = person["name"]                     #assign winner name to variable
+  #       binding.pry
+  #     end
       
+  #   end
+  # end
+  
+  
+  data[season].each do |person|
+    if person["status"] == "Winner"
+         winner = person["name"]                     
+        # binding.pry
     end
   end
+  
   winner.split(" ").shift
 end
 
@@ -20,7 +29,21 @@ end
 
 def get_contestant_name(data, occupation)
   # code here
+  name = ""
+  data.each do |season, contestants|
+    contestants.each do |person|
+      if person["occupation"] == occupation
+        name = person["name"]
+        #binding.pry
+      end
+    end
+  end
+  name
 end
+
+
+
+
 
 def count_contestants_by_hometown(data, hometown)
   # code here
